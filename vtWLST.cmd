834437117759d69a6a2452237fc1f0517d30c6e0
toyg <g.lacava@gmail.com>
2013-06-18 06:37:52 +0100

    added scripts to launch interactive console

---
+++ vtWLST.cmd
@@ -0,0 +1,13 @@
+rem SET VARIABLES
+set SCRIPT_HOME=%~dp0
+set SCRIPT=%SCRIPT_HOME%\scripts\verifyTargets.py
+set PROPS_FILE=%SCRIPT_HOME%\verifyTargets.properties
+FOR /F "tokens=1* delims==" %%P IN (%PROPS_FILE%) DO (set %%P=%%Q)
+
+rem add necessary java classes
+SET CLASSPATH=%CLASSPATH%;%MW_HOME%\oracle_common\modules;%MW_HOME%\oracle_common\modules\oracle.xdk_11.1.0
+
+set RUN_COMMAND=%MW_HOME%\oracle_common\common\bin\wlst.cmd
+set RUN_ARGS=-loadProperties %PROPS_FILE%
+
+%RUN_COMMAND% %RUN_ARGS%
---
+++ vtWLST.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# SET VARIABLES
+SCRIPT_HOME=`dirname $0`
+SCRIPT=${SCRIPT_HOME}/scripts/verifyTargets.py
+PROPS_FILE=${SCRIPT_HOME}/verifyTargets.properties
+. ${PROPS_FILE}
+
+SET CLASSPATH=%CLASSPATH%:%MW_HOME%/oracle_common/modules:%MW_HOME%/oracle_common/modules/oracle.xdk_11.1.0
+
+RUN_COMMAND=${MW_HOME}/oracle_common/common/bin/wlst.sh
+RUN_ARGS="-loadProperties ${PROPS_FILE}"
+
+${RUN_COMMAND} ${RUN_ARGS}
+
